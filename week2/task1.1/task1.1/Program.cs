d0c54ee08dc693b60cefe07f2b4bbbd871c3bcd9
Batko2403 <vip.sultanov24@gmail.com>
2019-02-15 15:32:29 +0300

    Lab2 Task1

---
+++ week2/task1.1/task1.1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Task_1_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создал переменную Pol типа bool
+
+            bool Pol = true;
+            //считываю данные из файла
+
+            using (FileStream fs = new FileStream(@"C:\Week2\Input.txt", FileMode.Open, FileAccess.Read))
+
+            {
+
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    //создаю переменную text типа string и сохраняю все элементы файла в texte. 
+                    //Потом эту переменную проверяю через функцию типа bool на палиндромность и записываю ответ в переменной типа bool
+                    //(если text палиндром то true,если не палиндром то false)
+
+                    string text = sr.ReadLine();
+
+                    Pol = Polindrome(text);
+
+                    if (Pol == true)
+
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+
+                    {
+                        Console.WriteLine("No");
+                    }
+
+                }
+            }
+            Console.ReadKey();
+        }
+        private static bool Polindrome(string text)
+
+        {
+            //создаю цикл чтобы пробежаться по элементам массива типа char
+            for (int i = 0; i < (text.Length) / 2; i++)
+            {
+                //здесь через условие проверяем 1 элемент массива и последний элемент массива.
+                //Если они одинаковы то переходим на следующий элемент. 
+                // А если они не одинаковые то выводим false и выходим из функции
+                if (text[i] != text[text.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
--- week2/task2/task2/Program.cs
+++ week2/task2/task2/Program.cs
@@ -1,107 +1,52 @@
 ﻿using System;
-
 using System.Collections.Generic;
-
 using System.IO;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
-
-
-
 namespace task2
-
 {
-
     class Program
-
     {
-
         static void Main(string[] args)
-
         {
-
             //создаю массив простых чисел
-
             int[] PrimeNumbers;
-
-
-
             //считываю данные из файла
-
             using (FileStream fs = new FileStream(@"D:\KBTU\PP2\week2\task2\task2.txt", FileMode.Open, FileAccess.Read))
-
             {
-
                 using (StreamReader sr = new StreamReader(fs))
-
                 {
-
                     //фильтр чисел
-
                     string text = sr.ReadLine();
-
                     int[] numbers = Numbers(text);
-
                     PrimeNumbers = IsPrime(numbers);
-
                 }
-
             }
-
             //записали результат на другой файл
-
             using (FileStream fs2 = new FileStream(@"D:\KBTU\PP2\week2\task2", FileMode.OpenOrCreate, FileAccess.Write))
-
             {
-
                 using (StreamWriter sw = new StreamWriter(fs2))
-
                 {
-
                     //запись простых чисел через пробел
-
                     foreach (var x in PrimeNumbers)
-
                     {
-
                         sw.Write(x + " ");
-
                     }
-
                 }
-
             }
-
         }
-
         private static int[] IsPrime(int[] num)
-
         {
-
             List<int> res = new List<int>();
-
-
-
             foreach (var x in num)
-
             {
-
                 int y = 0;
-
                 for (int i = 2; i * i <= x; i++)
-
                 {
-
                     if (x % i == 0)
-
                     {
-
                         y++;
-
                     }
 
                 }
