ee3bd71fc05ee3081a2a7c415cf799b1b91ffa59
Batko2403 <vip.sultanov24@gmail.com>
2019-02-15 16:57:50 +0300

    week 2

---
+++ week2/task4/task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           if (!File.Exists(@"C:\Users\Батыр\git\PP2\week2\task4\task4.txt"))
+            {
+                using (FileStream fs = new FileStream(@"C:\Users\Батыр\git\PP2\week2\task4\task4.txt", FileMode.CreateNew, FileAccess.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.Write("Hello World!");
+                    }
+                }
+            }
+            if (!Directory.Exists(@"C:\Users\Батыр\git\PP2\week2\task4\Batko"))
+            {
+                Directory.CreateDirectory(@"C:\Users\Батыр\git\PP2\week2\task4\Batko");
+            }
+            string TheFirstFile = Path.Combine(@"C:\Users\Батыр\git\PP2\week2\task4", "task4.txt");
+            string TheSecondFile = Path.Combine(@"C:\Users\Батыр\git\PP2\week2\task4\Batko", "task4.txt");
+
+
+            if (!File.Exists(@"C:\Users\Батыр\git\PP2\week2\task4\Batko\task4.txt"))
+
+            {
+
+                File.Move(@"C:\Users\Батыр\git\PP2\week2\task4\task4.txt", @"C:\Users\Батыр\git\PP2\week2\task4\Batko\task4.txt");
+
+            }
+
+        }
+
+    }
+
+}
